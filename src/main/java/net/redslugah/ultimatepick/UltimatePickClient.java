727775cdd072d9227ced5906b8c8816697d67184
Rafael Douglas <rafael.douglas.1@gmail.com>
2026-01-19 13:01:18 -0300

    Initial commit

---
+++ src/main/java/net/redslugah/ultimatepick/UltimatePickClient.java
@@ -0,0 +1,10 @@
+package net.redslugah.ultimatepick;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class UltimatePickClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/redslugah/ultimatepick/UltimatePickDataGenerator.java
@@ -0,0 +1,11 @@
+package net.redslugah.ultimatepick;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class UltimatePickDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
